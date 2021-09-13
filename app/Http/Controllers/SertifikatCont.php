ec438a18b4d080dc234a2df6d0980bb8b23f0a39
FebriTahta <febrirtah@gmail.com>
2021-09-13 16:35:43 +0700

    generate program id

--- app/Http/Controllers/SertifikatCont.php
+++ app/Http/Controllers/SertifikatCont.php
@@ -160,8 +160,8 @@ class SertifikatCont extends Controller
 
     public function generate_program_id(Request $request)
     {
-        $serti = Certificate::where('pelatihan_id',5090)->get();
-        $pelat = Pelatihan::where('id',5090)->first();
+        $serti = Certificate::where('pelatihan_id',5091)->get();
+        $pelat = Pelatihan::where('id',5091)->first();
         foreach ($serti as $key => $value) {
             # code...
             $value->tanggal = $pelat->tanggal;
