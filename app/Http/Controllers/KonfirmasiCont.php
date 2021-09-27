8eb271c1f93813389ecdebf77aec79581597712a
FebriTahta <febrirtah@gmail.com>
2021-09-27 10:37:49 +0700

    hapus tolakan

--- app/Http/Controllers/KonfirmasiCont.php
+++ app/Http/Controllers/KonfirmasiCont.php
@@ -183,7 +183,8 @@ class KonfirmasiCont extends Controller
                 curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, 0);
                 $result = curl_exec($curl);
                 curl_close($curl);
-                //
+                //hapus data peserta yang ditolak
+                $data->delete();
                 return response()->json(
                     [
                       'success' => 'Pendaftaran Peserta Telah Ditolak!',
