bc37e2d8dfdd0540798df68f154454f6c7d33508
FebriTahta <febrirtah@gmail.com>
2021-10-06 10:59:47 +0700

    update peserta

--- resources/views/tilawatipusat/peserta/update.blade.php
+++ resources/views/tilawatipusat/peserta/update.blade.php
@@ -1,9 +1,13 @@
 @extends('layouts.tilawatipusat_layouts.master')
 
 @section('content')
+@component('common-tilawatipusat.breadcrumb')
+@slot('title') UPDATE   @endslot
+@slot('title_li') PESERTA   @endslot
+@endcomponent
 <div class="row">
     <div class="form-group col-xl-6">
-        <label for="nama"></label>
+        <label for="nama">Nama Peserta</label>
         <input type="text" class="form-control" id="nama" name="name" required>    
     </div>    
 </div>    
