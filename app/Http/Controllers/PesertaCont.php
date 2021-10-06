6c6f31731d46190f0613111af05faf7481719d22
FebriTahta <febrirtah@gmail.com>
2021-10-06 09:38:01 +0700

    total peserta

--- app/Http/Controllers/PesertaCont.php
+++ app/Http/Controllers/PesertaCont.php
@@ -269,7 +269,7 @@ class PesertaCont extends Controller
             {
                 $data = DB::table('pesertas')
                 ->whereBetween('tanggal', array($request->dari, $request->sampai))
-                ->whereHas('prlatihan', function($query){
+                ->whereHas('pelatihan', function($query){
                     return $query->where('jenis','diklat');
                 })
                 ->get()->count();
@@ -277,7 +277,7 @@ class PesertaCont extends Controller
             }
             else
             {
-                $data = DB::table('pesertas')->whereHas('prlatihan', function($query){
+                $data = DB::table('pesertas')->whereHas('pelatihan', function($query){
                     return $query->where('jenis','diklat');
                 })
                 ->get()->count();
