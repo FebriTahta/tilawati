ac743d74d419c47b87c4c290a09fcd00f772a955
FebriTahta <febrirtah@gmail.com>
2021-10-06 10:50:06 +0700

    huruf besar expot

--- app/Http/Controllers/PesertaCont.php
+++ app/Http/Controllers/PesertaCont.php
@@ -96,7 +96,7 @@ class PesertaCont extends Controller
                             $actionBtn = ' <a href="#" data-id="'.$data->id.'" data-toggle="modal" data-target="#hapusData" class="btn btn-sm btn-outline btn-danger "><i class="fa fa-trash"></i></a>';
                             $actionBtn .= ' <a href="/diklat-profile-peserta/'.$data->id.'/'.$data->pelatihan->program->id.'/'.$data->pelatihan->id.'/admin" class="btn btn-sm btn-outline btn-info "><i class="fa fa-user"></i></a>';
                             $actionBtn .= ' <a href="#" class="btn btn-sm btn-outline btn-success" data-nama_peserta="'.$data->name.'" data-id="'.asset('images/'.$data->slug.'.png').'" data-toggle="modal" data-target=".modal-scan"><i class="mdi mdi-barcode-scan"></i></a>';
-                            $actionBtn .= ' <a href="#" data-toggle="modal" data-target="#modal-edit" class="btn btn-sm btn-outline btn-primary "><i class="fa fa-pencil-square"></i></a>';
+                            $actionBtn .= ' <a href="#" class="btn btn-sm btn-outline btn-primary "><i class="fa fa-pencil-square"></i></a>';
                             return $actionBtn;
                         })
                         ->addColumn('krits', function ($data) {
--- resources/views/tilawatipusat/peserta/index.blade.php
+++ resources/views/tilawatipusat/peserta/index.blade.php
@@ -281,47 +281,6 @@
                     </div>
 
                     <div class="col-sm-6 col-md-3 m-t-30">
-                        <div class="modal fade" id="modal-edit" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel" aria-hidden="true">
-                            <div class="modal-dialog modal-dialog-centered modal-lg">
-                                <div class="modal-content">
-                                    <div class="modal-body">
-                                        <div class="col-xl-12">
-                                            <div class="card m-b-30">
-                                                <div class="card-body">
-                                                    <div class="container-fluid">
-                                                        <form id="#"  method="POST" enctype="multipart/form-data">@csrf
-                                                            <div class="form-group">
-                                                                <h5>UBAH DATA PESERTA</h5>
-                                                                <input type="text" class="form-control text-capitalize" id="id" name="id" required>
-                                                            </div>
-                                                            <div class="form-group">
-                                                                <input type="text" class="form-control" placeholder="NAMA PESERTA" name="name" required>
-                                                            </div>
-                                                            <div class="form-group">
-                                                                <textarea name="alamat" class="form-control" id="alamat" cols="30" rows="2"></textarea>
-                                                            </div>
-                                                            <div class="row" style="text-align: center">
-                                                                <div class="form-group col-6 col-xl-6">
-                                                                    <input type="submit" name="ubah" id="btnubah" class="btn btn-info" value="Ya, Ubah!" />
-                                                                </div>
-                                                                <div class="form-group col-6 col-xl-6">
-                                                                    <button type="button" class="btn btn-secondary" data-dismiss="modal">
-                                                                        No, Cancel!
-                                                                    </button>
-                                                                </div>
-                                                            </div>
-                                                        </form>
-                                                    </div><!-- container fluid -->
-                                                </div>
-                                            </div>
-                                        </div> <!-- end col -->
-                                    </div>
-                                </div><!-- /.modal-content -->
-                            </div><!-- /.modal-dialog -->
-                        </div><!-- /.modal -->
-                    </div>
-
-                    <div class="col-sm-6 col-md-3 m-t-30">
                         <div class="modal fade bs-example-modal-kota" id="addkota" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel" aria-hidden="true">
                             <div class="modal-dialog modal-dialog-centered modal-md">
                                 <div class="modal-content">
