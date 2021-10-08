adbc9211a53fbf5dfa97f3985d50518c3e703168
FebriTahta <febrirtah@gmail.com>
2021-10-08 23:17:57 +0700

    cetak gelar

--- resources/views/AdmPelatihan/Cetak/cetak_depan.blade.php
+++ resources/views/AdmPelatihan/Cetak/cetak_depan.blade.php
@@ -41,18 +41,18 @@
 				<td class="atas" style="width: 11px; height: 10px;">:</td>
 				{{-- function penamaan gelar --}}
 				<?php 
-					public function ucname($str){
-						$string = ucwords(strtolower($str));
+					// public function ucname($str){
+						$string = ucwords(strtolower($item->name));
 						foreach (array('-','\'','.') as $delimiter) {
 							if (strpos($string, $delimiter) !== FALSE) {
 								$string = implode($delimiter, array_map('ucfirst', explode($delimiter, $string)));
 							}
 						}
 						return $string;
-					}
+					// }
 				?>
 				{{-- Output nama dengan gelar --}}
-				<td class="atas" style="width: 562px; height: 10px; font-weight: bold; text-transform: uppercase">{{ ucname($item->name) }}</td>
+				<td class="atas" style="width: 562px; height: 10px; font-weight: bold; text-transform: uppercase">{{ $string }}</td>
 				<td class="atas" style="width: 52px; height: 10px; ">&nbsp;</td>
 				</tr>
 				<tr class="atas" style="height: 10px;">
