69a2976184ccbfdb5d15c153789d98d52c8c0553
FebriTahta <febrirtah@gmail.com>
2021-10-12 14:11:22 +0700

    report peserta

--- app/Http/Controllers/PesertaCont.php
+++ app/Http/Controllers/PesertaCont.php
@@ -1493,20 +1493,17 @@ class PesertaCont extends Controller
         {
             if(!empty($request->dari))
             {
-                $data       = Peserta::where('jenis','diklat')->whereHas('program', function($query){
-                    $query->whereBetween('tanggal', array($request->dari, $request->sampai))->count();
-                });
-                $result1    = Peserta::where('bersyahadah',1)->whereBetween('tanggal', array($request->dari, $request->sampai))->count();
-                $result2    = ($data - $result1);
-                if ($result1 == 0) {
-                    # code...
-                    $hasil      = '<span class="text-info">'.$result1.'</span>'.' & '.'<span class="text-danger">'.$data.'</span>';
-                    return response()->json($hasil,200);
-                }else{
-                    # ada peserta yang lulus
-                    $hasil      = '<span class="text-info">'.$result1.'</span>'.' & '.'<span class="text-danger">'.$result2.'</span>';
-                    return response()->json($hasil,200);
-                }
+                $data = Peserta::whereBetween('tanggal', array($request->dari, $request->sampai))->whereHas('pelatihan', function($query){
+                    return $query->where('jenis','diklat');
+                })->get()->count();
+                $result1    = Peserta::whereBetween('tanggal', array($request->dari, $request->sampai))->where('bersyahadah',1)->whereHas('pelatihan', function($query){
+                    return $query->where('jenis','diklat');
+                })->get()->count();
+
+                $result2    = $data - $result1;
+                $hasil      = '<span class="text-info">'.$result1.'</span>'.' & '.'<span class="text-danger">'.$result2.'</span>';
+                return response()->json($hasil,200);
+
             }else{
                 $data = Peserta::whereHas('pelatihan', function($query){
                     return $query->where('jenis','diklat');
