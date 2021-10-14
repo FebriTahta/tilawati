bbfa054370614ab3ae7bea870455646c32ad994f
FebriTahta <febrirtah@gmail.com>
2021-10-14 23:53:13 +0700

    cetak satu surat pengiriman

--- resources/views/tilawatipusat/peserta/index.blade.php
+++ resources/views/tilawatipusat/peserta/index.blade.php
@@ -84,7 +84,7 @@
                                     <p class="card-title-desc">Ter-update berdasarkan Tahun 2021 </br></p>
                                     <a class="btn btn-sm btn-success  mr-1 text-white" style="width:130px" @if($diklat->program->penilaian->count() == 0) disabled @else href="{{ route('diklat.peserta_create', $pelatihan_id) }}" @endif><i class="mdi mdi-plus"></i> tambah peserta</a>
                                     <button class="btn btn-sm btn-success  mr-1" style="width:130px " data-toggle="modal" @if($diklat->program->penilaian->count() == 0) disabled @else data-target=".bs-example-modal-peserta" @endif><i class="mdi mdi-cloud-upload"></i> import peserta</button>
-                                    <button class="text-right float-right mr-1 btn btn-sm btn-outline-primary" id="cetak_all"><i class="fa fa-download"></i> download</button>
+                                    <button class="text-right float-right mr-1 btn btn-sm btn-outline-primary" id="cetak_all"><i class="fa fa-download"></i> cetak pengiriman modul</button>
 
                                     <input type="hidden" id="pelatihan_id" value="{{ $pelatihan_id }}">
                                     <blockquote class="blockquote font-size-16 mb-0 mt-2 table-responsive">
@@ -293,7 +293,7 @@
                                                         <form id="formcetaksurat" action="{{route('diklat.cetak_surat_pengiriman_beberapa')}}"  method="POST" enctype="multipart/form-data">@csrf
                                                             <div class="form-group text-center">
                                                                 <h5>CETAK SURAT PENGIRIMAN MODUL ?</h5>
-                                                                <input type="text" class="form-control text-capitalize" id="idcetaksurat" name="idcetaksurats" required>
+                                                                <input type="hidden" class="form-control text-capitalize" id="idcetaksurat" name="idcetaksurats" required>
                                                             </div>
                                                             <div class="row" style="text-align: center">
                                                                 <div class="form-group col-6 col-xl-6">
