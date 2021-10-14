1500eec965fccea5ac152a969bac99a0b4f519d5
FebriTahta <febrirtah@gmail.com>
2021-10-14 11:20:22 +0700

    update bersyahadah peserta

--- app/Http/Controllers/PesertaCont.php
+++ app/Http/Controllers/PesertaCont.php
@@ -1531,8 +1531,11 @@ class PesertaCont extends Controller
                                 'munaqisy'      => $request->munaqisy,
                             ]
                         );
-                        \QrCode::size(150)
-                        ->format('png') ->generate('https://www.profile.tilawatipusat.com/'.$peserta->slug, public_path('images/'.$peserta->slug.'.png'));
+                        // \QrCode::size(150)
+                        // ->format('png') ->generate('https://www.profile.tilawatipusat.com/'.$peserta->slug, public_path('images/'.$peserta->slug.'.png'));
+                        $qr = \QrCode::size(100)
+                        ->format('png')
+                        ->generate('https://www.profile.tilawatipusat.com/'.$slug, public_path('images/'.$slug.'.png'));
                         return redirect()->back()->with('success','Data Berasil Diperbarui');
     }
 
