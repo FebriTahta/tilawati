09fc1bf0677323a6293bfaaf112c4f5245c3d362
FebriTahta <febrirtah@gmail.com>
2021-10-14 23:35:12 +0700

    cetak satu surat pengiriman

--- app/Http/Controllers/CetakCont.php
+++ app/Http/Controllers/CetakCont.php
@@ -105,7 +105,7 @@ class CetakCont extends Controller
             $peserta_id_array = $request->allVals;
             $peserta= Peserta::whereIn('id', $peserta_id_array)->get();
             $pdf    = PDF::loadview('tilawatipusat.cetak.detail.surat_pengiriman',compact('peserta'))->setpaper('A4','portrait');
-            return $pdf->download('surat-pengiriman-'.$peserta->name.'-'.Carbon::parse($peserta->tanggal)->isoFormat('D-MMMM-Y').'.pdf');
+            return $pdf->download('surat-pengiriman-'.$peserta->program->name.'-'.Carbon::parse($peserta->program->tanggal)->isoFormat('D-MMMM-Y').'.pdf');
         }
     }
 
