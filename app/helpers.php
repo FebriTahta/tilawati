3fcdeb8f022260daa18d49ffaa492b0eebed3fd4
FebriTahta <febrirtah@gmail.com>
2021-10-14 09:51:35 +0700

    diklat import

--- app/helpers.php
+++ app/helpers.php
@@ -2,24 +2,18 @@
 
 function nama_gelar($str){
     $string = ucwords(strtolower($str));
-	
     foreach (array('-',',','.') as $key => $delimiter) {
         if (strpos($string, $delimiter) !== FALSE) {
             $string = implode($delimiter, array_map('ucfirst', explode($delimiter, $string)));
+		}
+		if(substr($str,0,2) == 'hj' || substr($str,0,2) == 'Hj' || substr($str,0,2) == 'HJ' || substr($str,0,2) == 'hJ' 
+		|| substr($str,0,2) == 'Dr' || substr($str,0,2) == 'dr' || substr($str,0,2) == 'dR' || substr($str,0,2) == 'DR'
+		)
+		{
+			return substr($string,0,2).strtoupper(substr($string,2,-5)).substr($string,-5);
+		}else{
 			return strtoupper(substr($string,0,-5)).substr($string,-5);
 		}
-		else{
-			return $string;
-		}
-    }
-	// if(substr($str,0,2) == 'hj' || substr($str,0,2) == 'Hj' || substr($str,0,2) == 'HJ' || substr($str,0,2) == 'hJ' 
-	// 	|| substr($str,0,2) == 'Dr' || substr($str,0,2) == 'dr' || substr($str,0,2) == 'dR' || substr($str,0,2) == 'DR'
-	// )
-	// {
-	// 	return substr($string,0,2).strtoupper(substr($string,2,-5)).substr($string,-5);
-	// }else{
-		
-	// }
-	// return $string;
+	}
 	
 }
