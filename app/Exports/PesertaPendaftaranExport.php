edf316239126464fada20f35a565109b5f6fc116
FebriTahta <febrirtah@gmail.com>
2021-10-15 09:53:17 +0700

    tanggal export

--- app/Exports/PesertaPendaftaranExport.php
+++ app/Exports/PesertaPendaftaranExport.php
@@ -48,9 +48,28 @@ class PesertaPendaftaranExport implements FromQuery, WithHeadings, ShouldAutoSiz
     //     return view('tilawatipusat.cetak.peserta.peserta_pendaftaran',compact('peserta'));
     // }
 
+    function nama_gelar($str){
+        $string = ucwords(strtolower($str));
+        $tanda	= array('-',',','.');
+        foreach ($tanda as $key => $delimiter) {
+            if (strpos($string, $delimiter) !== FALSE) {
+                $string = implode($delimiter, array_map('ucfirst', explode($delimiter, $string)));
+            }
+        }
+        if(substr($str,0,2) == 'hj' || substr($str,0,2) == 'Hj' || substr($str,0,2) == 'HJ' || substr($str,0,2) == 'hJ' 
+            || substr($str,0,2) == 'Dr' || substr($str,0,2) == 'dr' || substr($str,0,2) == 'dR' || substr($str,0,2) == 'DR'
+        )
+        {
+            return substr($string,0,2).strtoupper(substr($string,2,-5)).substr($string,-5);
+        }else{
+            return strtoupper(substr($string,0,-5)).substr($string,-5);
+        }
+        
+    }
+
     public function map($row): array{
         return [
-            $row->name,
+            nama_gelar($row->name),
             // \PhpOffice\PhpSpreadsheet\Shared\Date::excelToDateTimeObject(strtotime($row->tgllahir)),
             $row->program->name,
             $row->alamat,
