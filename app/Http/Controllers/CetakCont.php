412400b72a07075c7aa39f822ea0011c07595b48
FebriTahta <febrirtah@gmail.com>
2021-10-15 14:12:39 +0700

    tanggal export

--- app/Http/Controllers/CetakCont.php
+++ app/Http/Controllers/CetakCont.php
@@ -64,7 +64,7 @@ class CetakCont extends Controller
 
     public function cetak_depan_beberapa(Request $request)
     {
-        $peserta_id_array = $request->idcetaksurats;
+        $peserta_id_array = $request->id;
         $peserta    = Peserta::whereIn('id',explode(",",$peserta_id_array))->get();
         $customPaper = array(0,0,792,612);
         $pdf        = PDF::loadview('AdmPelatihan.Cetak.cetak_depan_beberapa',compact('peserta'))->setPaper($customPaper, 'portrait');
