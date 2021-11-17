3f6b0d0ae622d727d1f8a2522d68ef8a9ae1c4bc
FebriTahta <febrirtah@gmail.com>
2021-11-17 10:54:03 +0700

    cetak

--- app/Http/Controllers/CetakCont.php
+++ app/Http/Controllers/CetakCont.php
@@ -65,7 +65,7 @@ class CetakCont extends Controller
     public function cetak_depan_beberapa(Request $request)
     {
         $peserta_id_array = $request->id;
-        $peserta        = Peserta::whereIn('id',explode(",",$peserta_id_array))->get();
+        $peserta        = Peserta::whereIn('id',explode(",",$peserta_id_array))->where('kriteria','<>','')->get();
         $customPaper    = array(0,0,792,612);
         $pdf            = PDF::loadview('AdmPelatihan.Cetak.cetak_depan_beberapa',compact('peserta'))->setPaper($customPaper, 'portrait');
         return $pdf->download('ijazah-depan-peserta-pdf.pdf','I');
@@ -74,7 +74,7 @@ class CetakCont extends Controller
     public function cetak_belakang_beberapa(Request $request)
     {
         $peserta_id_array = $request->id;
-        $peserta        = Peserta::whereIn('id',explode(",",$peserta_id_array))->get();
+        $peserta        = Peserta::whereIn('id',explode(",",$peserta_id_array))->where('kriteria','<>','')->get();
         $customPaper = array(0,0,792,612);
     	$pdf = PDF::loadview('AdmPelatihan.Cetak.cetak_belakang_beberapa',compact('peserta'))->setPaper($customPaper, 'portrait');
     	return $pdf->download('ijazah-belakang-peserta.pdf','I');
