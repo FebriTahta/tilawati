334f5397aed14279b487a1a0e5dad2ffd07012ee
FebriTahta <febrirtah@gmail.com>
2021-11-30 10:24:22 +0700

    update tempat lahir

--- resources/views/tilawatipusat/peserta/update.blade.php
+++ resources/views/tilawatipusat/peserta/update.blade.php
@@ -40,8 +40,8 @@
             </select>
         </div>
         <div class="form-group col-12 col-xl-6">
-            <label for="tgllahir"><i class="text-danger">*</i> Tanggal Lahirs</label>
-            {{Carbon\Carbon::parse($peserta->tgllahir)->format('Y-m-d')}}
+            <label for="tgllahir"><i class="text-danger">*</i> Tanggal Lahir</label>
+            {{Carbon\Carbon::parse($peserta->tgllahir)->format('D MMMM Y')}}
             <input type="date" value="{{Carbon\Carbon::parse($peserta->tgllahir)->format('Y-m-d')}}" name="tgllahir" id="tgllahir" class="form-control" style="max-height: 40px; color: rgb(0, 0, 0); font-size: 13px;"  >
         </div>
         <div class="form-group col-12">
