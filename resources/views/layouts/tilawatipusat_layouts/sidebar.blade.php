61888a4e9e507a5985b5c9466ab27ebff0d2c33b
FebriTahta <febrirtah@gmail.com>
2021-12-13 09:14:46 +0700

    uppercase nama

--- resources/views/layouts/tilawatipusat_layouts/sidebar.blade.php
+++ resources/views/layouts/tilawatipusat_layouts/sidebar.blade.php
@@ -12,12 +12,12 @@
             <div class="mt-3">
 
                 <a href="#" class="text-dark font-weight-medium font-size-16"> 
-                    @if (auth()->user()->username == 'Pusat')
+                    @if (auth()->user()->username == 'pusat')
                         Tilawati Pusat (Nurul Falah)
                     @else
-                        {{ strtoupper(auth()->user()->username) }}
+                        {{ ucwords(auth()->user()->username) }}
                     @endif</a>
-                <p class="text-body mt-1 mb-0 font-size-13">{{ auth()->user()->role }}</p>
+                <p class="text-body mt-1 mb-0 font-size-13">{{ ucwords(auth()->user()->role) }}</p>
 
             </div>
         </div>
