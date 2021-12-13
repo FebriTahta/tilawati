5ad214978dcf8290a91eea9752369a167274ec69
FebriTahta <febrirtah@gmail.com>
2021-12-13 10:35:38 +0700

    laporan program data raker

--- resources/views/tilawatipusat/peserta/seluruh.blade.php
+++ resources/views/tilawatipusat/peserta/seluruh.blade.php
@@ -244,7 +244,7 @@
 
                     {{-- daftar program --}}
                     <div class="modal fade bs-example-modal-xl-2" id="mod_cabang3" tabindex="-1" role="dialog" aria-labelledby="myExtraLargeModalLabel" aria-hidden="true">
-                        <div class="modal-dialog modal-md">
+                        <div class="modal-dialog modal-lg">
                             <div class="modal-content">
                                 <div class="modal-header">
                                     <h5 class="modal-title mt-0" id="myExtraLargeModalLabel">DAFTAR PROGRAM DIKLAT</h5>
