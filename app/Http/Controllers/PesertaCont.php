0a0cd6a93f783d76a1379f61b10933949b458ed3
FebriTahta <febrirtah@gmail.com>
2021-12-13 15:33:36 +0700

    cabang raker

--- app/Http/Controllers/PesertaCont.php
+++ app/Http/Controllers/PesertaCont.php
@@ -1109,7 +1109,7 @@ class PesertaCont extends Controller
                 ->addColumn('action', function ($data) {
                     // $btn = '<a href="/diklat-peserta-diklat-cabang/'.$data->cabang->id.'" class="btn btn-sm btn-info"> check </a>';
                     $cabang = Cabang::where('id',$data->cabang_id)->first();
-                    $btn = $cabang->pelatihan->count();
+                    $btn = $cabang->pelatihan->get();
                     return $btn;
                 })
                 ->rawColumns(['cabang','action'])
