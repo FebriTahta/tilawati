7f340f3a6f02f18ed7387759484b09e5696a1673
FebriTahta <febrirtah@gmail.com>
2021-12-14 16:16:20 +0700

    data pelatihan cabang

--- app/Http/Controllers/PesertaCont.php
+++ app/Http/Controllers/PesertaCont.php
@@ -1171,7 +1171,7 @@ class PesertaCont extends Controller
                 
                 ->addColumn('jumlahdiklat', function($data){
                     
-                    return $data->pelatihan->count();
+                    return $data->pelatihan;
                 })
                 ->addColumn('namadiklat', function($data){
                     // foreach ($data->cabang->pelatihan as $key => $value) {
