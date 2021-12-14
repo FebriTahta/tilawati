1e9649ea609dc7e3d7f59a5ff3df48737fbf16cb
FebriTahta <febrirtah@gmail.com>
2021-12-14 10:50:03 +0700

    data cabang di laporan

--- app/Http/Controllers/PesertaCont.php
+++ app/Http/Controllers/PesertaCont.php
@@ -1133,7 +1133,7 @@ class PesertaCont extends Controller
                         $datax = Program::where('id',$value->program_id)->first();
                         $dataz[] = $datax->name;
                     }
-                    return implode($dataz);
+                    return implode($dataz).'</br>';
                 })
                 ->rawColumns(['cabang','jumlahdiklat','namadiklat'])->make(true);
                 
