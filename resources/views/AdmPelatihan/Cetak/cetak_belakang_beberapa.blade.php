5328ce26b1efe9457254221c20c717eaab8b70f6
FebriTahta <febrirtah@gmail.com>
2021-12-30 10:17:24 +0700

    cetak

--- resources/views/AdmPelatihan/Cetak/cetak_belakang_beberapa.blade.php
+++ resources/views/AdmPelatihan/Cetak/cetak_belakang_beberapa.blade.php
@@ -231,7 +231,7 @@
 				</div>
 				<table 
 				@if ($p->pelatihan->keterangan == 'instruktur')
-				style="width: 700px"
+				style="width: 700px; margin-left:154px"
 				@else
 				style="width: 782px; margin-left:154px"
 				@endif  class="table1">
