20e58fb6daf6fb1bde270deac7bb82b715c26e1f
FebriTahta <febrirtah@gmail.com>
2022-01-25 13:27:08 +0700

    tot

--- resources/views/AdmPelatihan/Cetak/cetak_belakang_beberapa.blade.php
+++ resources/views/AdmPelatihan/Cetak/cetak_belakang_beberapa.blade.php
@@ -496,7 +496,7 @@
 									<td class="nilaibawahtot" style="text-align: center">&nbsp; &nbsp;{{ $item->penilaian->max }}</td>
 									<td class="nilaibawahtot" style="text-align: center">&nbsp; &nbsp;{{ $item->penilaian->min }}</td>
 									<td class="nilaibawahtot"></td>
-									<th >{{ $item->nominal. $key }}</th>
+									<th >{{ $item->nominal. $tot[5] }}</th>
 									<?php $total += $item->nominal?>
 								</tr>
 								<?$x++?>
