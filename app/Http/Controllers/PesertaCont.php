c03312d3e39590b57e72dcdf1a7c5fafd6220e64
FebriTahta <febrirtah@gmail.com>
2022-01-25 11:40:03 +0700

    tot

--- app/Http/Controllers/PesertaCont.php
+++ app/Http/Controllers/PesertaCont.php
@@ -109,13 +109,7 @@ class PesertaCont extends Controller
                                         $penilaian3 = $data->nilai->where('penilaian_id', 32)->sum('nominal');
 
                                         $rata2 = ($total + $penilaian1 + $penilaian3)/3;
-                                        if ($rata2 > 70) {
-                                            # code...
-                                            return  '<a href="/diklat-nilai-edit/'.$data->id.'" data-id="'.$data->id.'" data-target="#nilaiPeserta" class="badge badge-info">'.$rata2.' BERSYAHADAH</a>';
-                                        } else {
-                                            # code...
-                                            return  '<a href="/diklat-nilai-edit/'.$data->id.'" data-id="'.$data->id.'" data-target="#nilaiPeserta" class="badge badge-warning">'.$rata2.' BELUM BERSYAADAH</a>';
-                                        }
+                                        return $rata2;
                                     } 
                                 } else {
                                     # code...
