94d019705dfe9f4b9eee4fc1046b0c9fa3e90532
FebriTahta <febrirtah@gmail.com>
2022-01-25 12:15:01 +0700

    tot

--- resources/views/AdmPelatihan/Cetak/cetak_belakang_beberapa.blade.php
+++ resources/views/AdmPelatihan/Cetak/cetak_belakang_beberapa.blade.php
@@ -443,6 +443,115 @@
 					<div class="alignright" style="margin-right: 210px">Cukup : 75 - 84</div>
 				</div>
 			</div>
+			@elseif($p->kriteria == 'SEBAGAI INSTRUKTUR LAGU METODE TILAWATI')
+			<div style="page-break-inside: avoid">
+				<div>
+					<p @if ($p->pelatihan->keterangan == 'instruktur') style="margin-top: 160px;margin-left: 358px" @else style="margin-top: 160px;margin-left: 358px" @endif class="syahadah">No. Syahadah : {{ $p->pelatihan->id }}/2021/{{ $p->id }}</p>
+				</div>
+				<table 
+				@if ($p->pelatihan->keterangan == 'instruktur')
+				style="width: 850px; margin-left:85px"
+				@else
+				style="width: 782px; margin-left:154px"
+				@endif  class="table1">
+					<tr>
+							<th rowspan="2">No.</th>
+							<th rowspan="2">Bidang Penilaian</th>
+							<th colspan="3" class="penilaian">Penilaian</th>
+							<th rowspan="2" style="text-align: center">Jumlah</th>
+					</tr>
+					<tr>     
+						<th class="pe">Max</th>
+						<th class="pe">Min</th>
+						<th class="pe">Nilai</th>
+						
+					</tr>
+					<tr>
+						<th>1</th>
+						<td>&nbsp; &nbsp;<b> Al-Qur'an</b></td>
+						<th colspan="3" class="pe3"></th>
+						<th >{{ $jumlah = $p->nilai->where("kategori","al-qur'an")->sum('nominal') }}</th>
+					</tr>
+					<?php $i = 2; $x = 1; $z = 5?>
+					@foreach ($p->nilai as $key=> $item)
+						@if ($item !== null)
+							@if ($item->kategori !== 'skill')
+								<tr>
+									<td class="pop"></td>
+									<td class="pop2" >&nbsp; &nbsp;&nbsp;<span style="text-transform: capitalize">{{ $item->penilaian->name }}</span></td>
+									<td class="nilai" style="text-align: center">&nbsp; &nbsp;{{ $item->penilaian->max }}</td>
+									<td class="nilai2" style="text-align: center">&nbsp; &nbsp;{{ $item->penilaian->min}}</td>
+									<td class="nilai3" style="text-align: center">&nbsp; &nbsp;{{ $item->nominal }}</td>
+									<td style="border-top: 0;border-bottom: 0;"></td>
+								</tr>
+								<?php $z--; ?>
+							@else
+								<tr>
+									<th>{{ $i++ }}</th>
+									<td class="nilai6" style="text-transform: uppercase">&nbsp; &nbsp;<b> {{ $item->penilaian->name }}</b></th>
+									{{-- <th colspan="3" class="nilai5"></th> --}}
+									<td class="nilaibawahtot" style="text-align: center">&nbsp; &nbsp;{{ $item->penilaian->max }}</td>
+									<td class="nilaibawahtot" style="text-align: center">&nbsp; &nbsp;{{ $item->penilaian->min }}</td>
+									<td class="nilaibawahtot"></td>
+									<th >{{ $item->nominal }}</th>
+									<?php $total += $item->nominal?>
+								</tr>
+								<?$x++?>
+							@endif
+						@else
+							{{--  --}}
+						@endif
+					@endforeach
+					@if ($p->pelatihan->keterangan == 'guru')
+						<tr>
+							<th></th>
+							<td class="nilai6">&nbsp; &nbsp;<b> RATA - RATA NILAI</b></th>
+							<th colspan="3" class="nilai5"></th>
+							<th >
+							@if ($p->pelatihan->program->name=='munaqosyah santri')
+								{{ $rata2 = $jumlah }}
+							@else
+								{{ $rata2 = ($jumlah+ $item->nominal)/2 }}
+							@endif
+								</th>
+						</tr>
+					@else
+					<?php 
+						$rata2 = $jumlah;
+
+					?>
+					<tr>
+						<th></th>
+						<td class="nilai6">&nbsp; &nbsp;<b> RATA - RATA NILAI</b></th>
+						<th colspan="3" class="nilai5"></th>
+						<th >{{ $rata2 = ($jumlah+$total)/4 }}</th>
+					</tr>
+					@endif
+					<tr>
+						<th></th>
+						<td class="nilai6">&nbsp; &nbsp;<b> PRESTASI</b></th>
+						<th colspan="3" class="nilai5"></th> 
+						<th >
+							{{-- @if ($x !== 1)
+								@if ($rata2 >= 85)
+									Baik
+								@else
+									Cukup
+								@endif
+							@endif --}}
+							@if ($rata2 >= 85)
+								Baik
+							@else
+								Cukup
+							@endif
+						</th>
+					</tr>
+				</table>
+				<div id="textbox" style="margin-top: 20px">
+					<div class="alignleft" style="margin-left: 180px">Baik : 85 - 95</div>
+					<div class="alignright" style="margin-right: 210px">Cukup : 75 - 84</div>
+				</div>
+			</div>
 			@endif
 		@else
 		{{-- Selain TOT --}}
