d87a6c48dd15485da52dbc2426c06b942df822df
FebriTahta <febrirtah@gmail.com>
2022-01-25 12:24:43 +0700

    tot

--- resources/views/AdmPelatihan/Cetak/cetak_belakang_beberapa.blade.php
+++ resources/views/AdmPelatihan/Cetak/cetak_belakang_beberapa.blade.php
@@ -487,8 +487,8 @@
 								<?php $z--; ?>
 							@else
 								<tr>
-									
-									<th>{{ $i++ }} {{$i}}</th>
+									<?php $yah[$i] = $i?>
+									<th>{{$yah}} {{ $i++ }}</th>
 									<td class="nilai6" style="text-transform: uppercase">&nbsp; &nbsp;<b> {{ $item->penilaian->name }}</b></th>
 									{{-- <th colspan="3" class="nilai5"></th> --}}
 									<td class="nilaibawahtot" style="text-align: center">&nbsp; &nbsp;{{ $item->penilaian->max }}</td>
