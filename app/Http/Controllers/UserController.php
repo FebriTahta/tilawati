7212fbaaa02581193aad3181625aa3e491f4427d
FebriTahta <febrirtah@gmail.com>
2022-02-04 08:47:54 +0700

    oke

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -39,56 +39,65 @@ class UserController extends Controller
 
     public function reset_password(Request $request)
     {
-        // $user = User::where('role','cabang')->get();
-        // foreach ($user as $key => $value) {
-        //     # code...
-        //     $pool = '0123456789';
-        //     $acak = substr(str_shuffle(str_repeat($pool, 5)), 0, 3);
-        //     if (condition) {
-        //         # code...
-        //     }
-        //     $value->update([
-        //         'pass'=> 'cab'.$acak,
-        //         'password'=>Hash::make('cab'.$acak)
-        //     ]);
-        // }
-
-        // return redirect()->back();
-
-        $characters = '0123456789abcdefghiklmnopqrstuvwxyz';
-        $charactersNumber = strlen($characters);
-        $codeLength = 3;
-
-        $code = '';
-
-        while (strlen($code) < 3) {
-            $position = rand(0, $charactersNumber - 1);
-            $character = $characters[$position];
-            $code = 'cab'.$code.$character;
-        }
-
-        $data = User::where('role','cabang')->get();
-        foreach ($data as $key => $value) {
+        $user = User::where('role','cabang')->get();
+        foreach ($user as $key => $value) {
             # code...
-            if ($value::where('pass', $code)->exists()) {
-                // $this->generateUniqueCode();
-                while (strlen($code) < 3) {
-                    $position = rand(0, $charactersNumber - 1);
-                    $character = $characters[$position];
-                    $code = 'cab'.$code.$character;
-                }
+            $pool = '0123456789';
+            $acak = substr(str_shuffle(str_repeat($pool, 5)), 0, 3);
+            if ($value->where('pass', 'cab'.$acak)->exists()) {
+                # code...
+                $pool2 = '0123456789';
+                $acak2 = substr(str_shuffle(str_repeat($pool2, 5)), 0, 3);
                 $value->update([
-                            'pass'=> $code,
-                            'password'=>Hash::make($code)
-                        ]);
+                    'pass'=> 'cab'.$acak2,
+                    'password'=>Hash::make('cab'.$acak2)
+                ]);
             }else {
                 # code...
                 $value->update([
-                    'pass'=> $code,
-                    'password'=>Hash::make($code)
+                    'pass'=> 'cab'.$acak,
+                    'password'=>Hash::make('cab'.$acak)
                 ]);
             }
+            
         }
+
+        return redirect()->back();
+
+        // $characters = '0123456789abcdefghiklmnopqrstuvwxyz';
+        // $charactersNumber = strlen($characters);
+        // $codeLength = 3;
+
+        // $code = '';
+
+        // while (strlen($code) < 3) {
+        //     $position = rand(0, $charactersNumber - 1);
+        //     $character = $characters[$position];
+        //     $code = 'cab'.$code.$character;
+        // }
+
+        // $data = User::where('role','cabang')->get();
+        // foreach ($data as $key => $value) {
+        //     # code...
+        //     if ($value::where('pass', $code)->exists()) {
+        //         // $this->generateUniqueCode();
+        //         while (strlen($code) < 3) {
+        //             $position = rand(0, $charactersNumber - 1);
+        //             $character = $characters[$position];
+        //             $code = 'cab'.$code.$character;
+        //         }
+        //         $value->update([
+        //                     'pass'=> $code,
+        //                     'password'=>Hash::make($code)
+        //                 ]);
+        //     }else {
+        //         # code...
+        //         $value->update([
+        //             'pass'=> $code,
+        //             'password'=>Hash::make($code)
+        //         ]);
+        //     }
+        // }
 
         
     }
