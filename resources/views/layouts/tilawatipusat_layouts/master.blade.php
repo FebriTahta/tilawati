9d3c1aa18e23251ebf4783de6437d17e8279d7cd
FebriTahta <febrirtah@gmail.com>
2022-02-10 16:03:57 +0700

    dashboard

--- resources/views/layouts/tilawatipusat_layouts/master.blade.php
+++ resources/views/layouts/tilawatipusat_layouts/master.blade.php
@@ -11,6 +11,13 @@
     <!-- App favicon -->
     <link rel="shortcut icon" href="{{ URL::asset('tilawatipusat/images/favicon.ico')}}">
     @include('layouts.tilawatipusat_layouts.head')
+    <!-- App css -->
+    <link href="{{ URL::asset('tilawatipusat/css/bootstrap-dark.min.css')}}" id="bootstrap-dark" rel="stylesheet" type="text/css" />
+    <link href="{{ URL::asset('tilawatipusat/css/bootstrap.min.css')}}" id="bootstrap-light" rel="stylesheet" type="text/css" />
+    <link href="{{ URL::asset('tilawatipusat/css/icons.min.css')}}" rel="stylesheet" type="text/css" />
+    <link href="{{ URL::asset('tilawatipusat/css/app-rtl.min.css')}}" id="app-rtl" rel="stylesheet" type="text/css" />
+    <link href="{{ URL::asset('tilawatipusat/css/app-dark.min.css')}}" id="app-dark" rel="stylesheet" type="text/css" />
+    <link href="{{ URL::asset('tilawatipusat/css/app.min.css')}}" id="app-light" rel="stylesheet" type="text/css" />
 </head>
 
 @section('body')
