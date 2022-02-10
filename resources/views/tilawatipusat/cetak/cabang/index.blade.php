85c9f8876220801d162a4d71aa10a5fdc2b206d1
FebriTahta <febrirtah@gmail.com>
2022-02-10 10:24:32 +0700

    trainer

--- resources/views/tilawatipusat/cetak/cabang/index.blade.php
+++ resources/views/tilawatipusat/cetak/cabang/index.blade.php
@@ -1,30 +1,41 @@
-<style>
-    .notel {
-    mso-number-format: "\@";
-    }
-</style>
-<table>
-    <thead style="font-weight: bold">
-        <tr>
-            <th colspan="5">WOOOOOO</th>
-        </tr>
-    </thead>
-</table>
-<table>
-    <thead style="font-weight: bold;">
-        <tr style="border: solid; text-decoration: uppercase">
-            <th>NO</th>
-            <th>Nama Lengkap Peserta</th>
-            <th>Tanggal Pelatihan</th>
-            <th>Diklat</th>
-            <th>Alamat Lengkap</th>
-            <th>Kota / Kabupaten Asal</th>
-            <th>No WA</th>
-            <th>Tempat Lahir</th>
-            <th>Tanggal Lahir</th>
-        </tr>
-    </thead >
-    <tbody style="text-transform: uppercase; font-size: 12px">
-        
-    </tbody>
-</table>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <style>
+        .notel {
+        mso-number-format: "\@";
+        }
+    </style>
+</head>
+<body>
+    <table>
+        <thead style="font-weight: bold">
+            <tr>
+                <th colspan="5">WOOOOOO</th>
+            </tr>
+        </thead>
+    </table>
+    <table>
+        <thead style="font-weight: bold;">
+            <tr style="border: solid; text-decoration: uppercase">
+                <th>NO</th>
+                <th>Nama Lengkap Peserta</th>
+                <th>Tanggal Pelatihan</th>
+                <th>Diklat</th>
+                <th>Alamat Lengkap</th>
+                <th>Kota / Kabupaten Asal</th>
+                <th>No WA</th>
+                <th>Tempat Lahir</th>
+                <th>Tanggal Lahir</th>
+            </tr>
+        </thead >
+        <tbody style="text-transform: uppercase; font-size: 12px">
+            
+        </tbody>
+    </table>
+</body>
+</html>
