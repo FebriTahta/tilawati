0996303b174db616680d73462f0feacc5c11b0bb
FebriTahta <febrirtah@gmail.com>
2022-02-10 10:54:39 +0700

    cetak cabang

--- resources/views/tilawatipusat/cetak/cabang/index.blade.php
+++ resources/views/tilawatipusat/cetak/cabang/index.blade.php
@@ -51,8 +51,8 @@
                 ->select('trainer')->distinct()->get();?>
                 <td>
                     @foreach ($trainers as $value)
-                    {{-- $tot_train = Trainer::where('cabang_id', $data->id)->where('trainer',$value->trainer)->count(); --}}
-                    <br>{{'- '.$value->trainer}}
+                    <?php $tot_train = Trainer::where('cabang_id', $data->id)->where('trainer',$value->trainer)->count();?>
+                    <br>{{ $tot_train.' - '.$value->trainer }}
                     @endforeach
                 </td>
             </tr>
