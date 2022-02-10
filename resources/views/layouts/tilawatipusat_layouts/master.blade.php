b66822da6b3ce4aa076e18f60b728f303e804df7
FebriTahta <febrirtah@gmail.com>
2022-02-10 16:05:27 +0700

    dashboard

--- resources/views/layouts/tilawatipusat_layouts/master.blade.php
+++ resources/views/layouts/tilawatipusat_layouts/master.blade.php
@@ -9,19 +9,23 @@
     <meta content="Themesbrand" name="author" />
     <meta name="csrf-token" content="{{ csrf_token() }}" />
     <!-- App favicon -->
-    <link rel="shortcut icon" href="{{ URL::asset('tilawatipusat/images/favicon.ico')}}">
-    @include('layouts.tilawatipusat_layouts.head')
-    <!-- App css -->
-    <link href="{{ URL::asset('tilawatipusat/css/bootstrap-dark.min.css')}}" id="bootstrap-dark" rel="stylesheet" type="text/css" />
-    <link href="{{ URL::asset('tilawatipusat/css/bootstrap.min.css')}}" id="bootstrap-light" rel="stylesheet" type="text/css" />
-    <link href="{{ URL::asset('tilawatipusat/css/icons.min.css')}}" rel="stylesheet" type="text/css" />
-    <link href="{{ URL::asset('tilawatipusat/css/app-rtl.min.css')}}" id="app-rtl" rel="stylesheet" type="text/css" />
-    <link href="{{ URL::asset('tilawatipusat/css/app-dark.min.css')}}" id="app-dark" rel="stylesheet" type="text/css" />
-    <link href="{{ URL::asset('tilawatipusat/css/app.min.css')}}" id="app-light" rel="stylesheet" type="text/css" />
+    <link href="{{ URL::asset('tilawatipusat/css/bootstrap-dark.min.css') }}" id="bootstrap-dark" rel="stylesheet"
+        type="text/css" />
+    <link href="{{ URL::asset('tilawatipusat/css/bootstrap.min.css') }}" id="bootstrap-light" rel="stylesheet"
+        type="text/css" />
+    <link href="{{ URL::asset('tilawatipusat/css/icons.min.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ URL::asset('tilawatipusat/css/app-rtl.min.css') }}" id="app-rtl" rel="stylesheet" type="text/css" />
+    <link href="{{ URL::asset('tilawatipusat/css/app-dark.min.css') }}" id="app-dark" rel="stylesheet"
+        type="text/css" />
+    <link href="{{ URL::asset('tilawatipusat/css/app.min.css') }}" id="app-light" rel="stylesheet" type="text/css" />
+    <link rel="shortcut icon" href="{{ URL::asset('tilawatipusat/images/favicon.ico') }}">
+    {{-- @include('layouts.tilawatipusat_layouts.head') --}}
+
 </head>
 
 @section('body')
 @show
+
 <body data-layout="detached" data-topbar="colored">
     <div id="preloader">
         <div id="status">
