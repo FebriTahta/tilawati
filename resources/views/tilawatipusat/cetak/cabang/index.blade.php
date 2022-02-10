eb7138de35efc724eae3c26a013b86a2b7b78473
FebriTahta <febrirtah@gmail.com>
2022-02-10 10:31:49 +0700

    cetak cabang

--- resources/views/tilawatipusat/cetak/cabang/index.blade.php
+++ resources/views/tilawatipusat/cetak/cabang/index.blade.php
@@ -13,25 +13,25 @@
 </head>
 <body>
     <table>
-        <thead style="font-weight: bold">
+        <thead style="font-weight: bold; text-transform: uppercase">
             <tr>
-                <th colspan="5">WOOOOOO</th>
+                <th rowspan="3" colspan="10">DATA CABANG <br> <small>Seluruh Indonesia Per - 2022</small></th>
             </tr>
         </thead>
     </table>
     <table>
         <thead style="font-weight: bold;">
             <tr style="border: solid; text-transform: uppercase">
-                <th colspan="2">NO</th>
-                <th colspan="2">NAMA CABANG</th>
-                <th colspan="2">NAMA KEPALA CABANG</th>
-                <th colspan="2">Kadivre</th>
-                <th colspan="2">Alamat Sekretariat</th>
-                <th colspan="2">Kota</th>
-                <th colspan="2">Provinsi</th>
-                <th colspan="2">Wilayah</th>
-                <th colspan="2">Telpon</th>
-                <th colspan="2">Email</th>
+                <th rowspan="2">NO</th>
+                <th rowspan="2">NAMA CABANG</th>
+                <th rowspan="2">NAMA KEPALA CABANG</th>
+                <th rowspan="2">Kadivre</th>
+                <th rowspan="2">Alamat Sekretariat</th>
+                <th rowspan="2">Kota</th>
+                <th rowspan="2">Provinsi</th>
+                <th rowspan="2">Wilayah</th>
+                <th rowspan="2">Telpon</th>
+                <th rowspan="2">Email</th>
             </tr>
         </thead >
         <tbody style="text-transform: uppercase; font-size: 12px">
