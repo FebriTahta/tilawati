6dc5addd5be035faeada4014bf37df6ad3bec87d
FebriTahta <febrirtah@gmail.com>
2022-02-11 15:08:58 +0700

    oke

--- resources/views/tilawatipusat/lembaga/index.blade.php
+++ resources/views/tilawatipusat/lembaga/index.blade.php
@@ -417,6 +417,7 @@
                 var button = $(event.relatedTarget)
                 var id = button.data('id')
                 var modal = $(this)
+                console.log(id);
                 modal.find('.modal-body #id').val(id);
             })
             $('#hapuslembaga').submit(function(e) {
