d08ecbcdc3cd7cdc62948c281e2866e10197fb86
FebriTahta <febrirtah@gmail.com>
2022-02-11 15:06:17 +0700

    oke

--- resources/views/tilawatipusat/lembaga/index.blade.php
+++ resources/views/tilawatipusat/lembaga/index.blade.php
@@ -368,7 +368,7 @@
                                     <form id="hapuslembaga"  method="POST" enctype="multipart/form-data">@csrf
                                         <div class="form-group text-center">
                                             <h5>Anda yakin akan menghapus Diklat tersebut ?</h5>
-                                            <input type="hidden" class="form-control text-capitalize" id="id" name="id" required>
+                                            <input type="text" class="form-control text-capitalize" id="id" name="id" required>
                                         </div>
                                         <div class="row" style="text-align: center">
                                             <div class="form-group col-6 col-xl-6">
