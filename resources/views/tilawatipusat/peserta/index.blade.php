44ad3a3734bcb475e063df1f5b85ab1dd73dd256
FebriTahta <febrirtah@gmail.com>
2022-02-11 13:40:48 +0700

    oke

--- resources/views/tilawatipusat/peserta/index.blade.php
+++ resources/views/tilawatipusat/peserta/index.blade.php
@@ -85,11 +85,15 @@
                                     {{-- <p class="card-title-desc">Ter-update berdasarkan Tahun 2021 </br></p> --}}
                                     <a class="btn btn-sm btn-outline-success  mb-1 mr-1" style="width:130px" @if($diklat->program->penilaian->count() == 0) disabled @else href="{{ route('diklat.peserta_create', $pelatihan_id) }}" @endif><i class="mdi mdi-plus"></i> tambah peserta</a>
                                     <button class="btn btn-sm btn-outline-success  mb-1 mr-1" style="width:130px " data-toggle="modal" @if($diklat->program->penilaian->count() == 0) disabled @else data-target=".bs-example-modal-peserta" @endif><i class="mdi mdi-cloud-upload"></i> import peserta</button>
+                                    @if (auth()->user()->role=='pusat')
                                     <button class="text-right btn btn-sm mr-1 mb-1 btn-outline-primary" id="cetak_all"><i class="fa fa-download"></i> pengiriman modul</button>
                                     <button class="text-right btn btn-sm mr-1 mb-1 btn-outline-info" id="depan_all"><i class="fa fa-print"></i> depan</button>
-                                    <button class="text-right btn btn-sm mr-1 mb-1 btn-outline-info" id="belakang_all"><i class="fa fa-print"></i> belakang</button>
-                                    <button class="text-right btn btn-sm mr-1 mb-1 btn-outline-danger" id="hapus_all"><i class="fa fa-trash"></i> hapus data</button>
+                                    <button class="text-right btn btn-sm mr-1 mb-1 btn-outline-info" id="belakang_all"><i class="fa fa-print"></i> belakang</button>    
+                                    
                                     <button class="text-right btn btn-sm mr-1 mb-1 btn-outline-secondary" id="depan_lama_all"><i class="fa fa-print"></i> depan versi lama</button>
+                                    @endif
+                                    
+                                    <button class="text-right btn btn-sm mr-1 mb-1 btn-outline-danger" id="hapus_all"><i class="fa fa-trash"></i> hapus data</button>
                                     {{-- <form action="/error-penilaian-kategori" method="POST">@csrf
                                         <button type="submit" class="text-right btn btn-sm mr-1 btn-outline-info" id="belakang_all"><i class="fa fa-print"></i> belakang</button>
                                     </form> --}}
