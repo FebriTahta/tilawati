4a4648202da4d31763831f888816106ac8f6c8a1
FebriTahta <febrirtah@gmail.com>
2022-02-13 18:21:00 +0700

    oke

--- app/Http/Controllers/NilaiCont.php
+++ app/Http/Controllers/NilaiCont.php
@@ -149,24 +149,27 @@ class NilaiCont extends Controller
                 $total  = $peserta->nilai->where("kategori","al-qur'an")->sum('nominal');
                 $penilaian2 = $peserta->nilai->where('penilaian_id', 31)->sum('nominal');
                 $penilaian3 = $peserta->nilai->where('penilaian_id', 32)->sum('nominal');
+                $rata1 = $total;
                 $rata2 = ($total + $penilaian2 + $penilaian3)/3;
                 $kriteria   = Kriteria::where('program_id', $peserta->program_id)->get();
-                return view('tilawatipusat.nilai.edit',compact('peserta','total','rata2','kriteria'));
+                return view('tilawatipusat.nilai.edit',compact('peserta','total','rata2','kriteria','rata1'));
 
             }elseif($peserta->kriteria == 'SEBAGAI INSTRUKTUR STRATEGI MENGAJAR METODE TILAWATI'){
                 $total  = $peserta->nilai->where("kategori","al-qur'an")->sum('nominal');
                 $penilaian1 = $peserta->nilai->where('penilaian_id', 30)->sum('nominal');
                 $penilaian3 = $peserta->nilai->where('penilaian_id', 32)->sum('nominal');
                 $kriteria   = Kriteria::where('program_id', $peserta->program_id)->get();
+                $rata1 = $total;
                 $rata2 = ($total + $penilaian1 + $penilaian3)/3;
-                return view('tilawatipusat.nilai.edit',compact('peserta','total','rata2','kriteria'));
+                return view('tilawatipusat.nilai.edit',compact('peserta','total','rata2','kriteria','rata1'));
             }else{
                 $total      = $peserta->nilai->where("kategori","al-qur'an")->sum('nominal');
                 $total2     = $peserta->nilai->where("kategori","skill")->sum('nominal');
                 $total3     = $peserta->nilai->where("kategori","skill")->count();
+                $rata1 = $total;
                 $rata2      = ($total + $total2)/($total3+1);
                 $kriteria   = Kriteria::where('program_id', $peserta->program_id)->get();
-                return view('tilawatipusat.nilai.edit',compact('peserta','total','rata2','kriteria'));
+                return view('tilawatipusat.nilai.edit',compact('peserta','total','rata2','kriteria','rata1'));
             }
             
         } else {
