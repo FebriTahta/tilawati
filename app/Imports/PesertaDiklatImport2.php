88be4e7c05def10eda3dccdf5f1c65b155aa9280
FebriTahta <febrirtah@gmail.com>
2022-02-13 11:20:51 +0700

    ok

--- app/Imports/PesertaDiklatImport2.php
+++ app/Imports/PesertaDiklatImport2.php
@@ -60,6 +60,7 @@ class PesertaDiklatImport2 implements ToCollection, WithStartRow
             if ($peserta == null) {
                 # code...
                 $dt_pel = new Peserta;
+                $dt_pel->name = $row[0];
                 $dt_pel->jilid =$row[7];
                 $dt_pel->created_at = new \DateTime;
                 $dt_pel->save();
