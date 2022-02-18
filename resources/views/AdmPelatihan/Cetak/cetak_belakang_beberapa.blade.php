7a5204b201590d55eb610a1e8bd51c3da37e1c2f
FebriTahta <febrirtah@gmail.com>
2022-02-17 19:24:47 -0800

    oke

--- resources/views/AdmPelatihan/Cetak/cetak_belakang_beberapa.blade.php
+++ resources/views/AdmPelatihan/Cetak/cetak_belakang_beberapa.blade.php
@@ -257,7 +257,7 @@
 							@if ($item->kategori !== 'skill')
 								<tr>
 									<td class="pop"></td>
-									<td class="pop2" >&nbsp; &nbsp;&nbsp;<span style="text-transform: capitalize">{{ $item->penilaian }}</span></td>
+									<td class="pop2" >&nbsp; &nbsp;&nbsp;<span style="text-transform: capitalize">{{ $item->penilaian->name }}</span></td>
 									<td class="nilai" style="text-align: center">&nbsp; &nbsp;{{ $item->penilaian->max }}</td>
 									<td class="nilai2" style="text-align: center">&nbsp; &nbsp;{{ $item->penilaian->min}}</td>
 									<td class="nilai3" style="text-align: center">&nbsp; &nbsp;{{ $item->nominal }}</td>
@@ -529,7 +529,7 @@
 						@if ($item->kategori !== 'skill')
 							<tr>
 								<td class="pop"></td>
-								<td class="pop2" >&nbsp; &nbsp;&nbsp;<span style="text-transform: capitalize">{{ $item->penilaian->name }}</span></td>
+								<td class="pop2" >&nbsp; &nbsp;&nbsp;<span style="text-transform: capitalize">{{ $item->penilaian }}</span></td>
 								<td class="nilai" style="text-align: center">&nbsp; &nbsp;{{ $item->penilaian->max }}</td>
 								<td class="nilai2" style="text-align: center">&nbsp; &nbsp;{{ $item->penilaian->min }}</td>
 								<td class="nilai3" style="text-align: center">&nbsp; &nbsp;{{ $item->nominal }}</td>
