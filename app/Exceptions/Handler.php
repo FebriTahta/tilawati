c44e35371a2f31c8f0f6526b68c910669edb49b5
FebriTahta <febrirtah@gmail.com>
2022-02-20 19:50:42 -0800

    oke

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -46,7 +46,8 @@ class Handler extends ExceptionHandler
                 return response()->view('errors.' . '404', [], 404);
             }
             if ($exception->getStatusCode() == 500) {
-                return response()->view('errors.' . '500', [], 500);
+                // return response()->view('errors.' . '500', [], 500);
+                return view('errors.500');
             }
         }
         return parent::render($request, $exception);
