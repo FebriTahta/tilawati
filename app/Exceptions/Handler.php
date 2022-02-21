8ecd63ece46f2b3f17ea4b401cec5788dff43f75
FebriTahta <febrirtah@gmail.com>
2022-02-20 19:51:14 -0800

    oke

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -39,17 +39,17 @@ class Handler extends ExceptionHandler
         });
     }
 
-    public function render($request, Exception $exception)
-    {
-        if ($this->isHttpException($exception)) {
-            if ($exception->getStatusCode() == 404) {
-                return response()->view('errors.' . '404', [], 404);
-            }
-            if ($exception->getStatusCode() == 500) {
-                // return response()->view('errors.' . '500', [], 500);
-                return view('errors.500');
-            }
-        }
-        return parent::render($request, $exception);
-    }
+    // public function render($request, Exception $exception)
+    // {
+    //     if ($this->isHttpException($exception)) {
+    //         if ($exception->getStatusCode() == 404) {
+    //             return response()->view('errors.' . '404', [], 404);
+    //         }
+    //         if ($exception->getStatusCode() == 500) {
+    //             // return response()->view('errors.' . '500', [], 500);
+    //             return view('errors.500');
+    //         }
+    //     }
+    //     return parent::render($request, $exception);
+    // }
 }
