3867e8e8aefd775dbab64423e1f138d0d1684d89
FebriTahta <febrirtah@gmail.com>
2022-02-28 19:38:55 -0800

    oke

--- app/Exports/PesertaPendaftaranExport.php
+++ app/Exports/PesertaPendaftaranExport.php
@@ -91,7 +91,7 @@ class PesertaPendaftaranExport implements FromQuery, WithHeadings, ShouldAutoSiz
         
         if (DateTime::createFromFormat('Y-m-d', $row->tgllahir) !== false) {
             # code...
-            $tgl_lahir = Carbon::parse($row->tgllahir)->format('d/MM/Y');
+            $tgl_lahir = Carbon::parse($row->tgllahir)->format('d/mm/Y');
         }else{
             $tgl_lahir = $row->tgllahir;
         }
