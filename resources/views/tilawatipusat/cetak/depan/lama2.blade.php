3dbf4ec6483916aed17883a2f58d67a0daa91185
FebriTahta <febrirtah@gmail.com>
2022-03-16 21:33:57 -0700

    table

--- resources/views/tilawatipusat/cetak/depan/lama2.blade.php
+++ resources/views/tilawatipusat/cetak/depan/lama2.blade.php
@@ -30,7 +30,7 @@
 		@foreach ($peserta as $item)
 		<div class="print">
 			<table 
-			style="height: 180px; width: 900px;margin-left:138px;margin-top:275px"
+			style="height: 180px; width: 900px;margin-left:138px;margin-top:264px"
 			class="dalam">
 				<tbody>
 				<tr class="atas">
