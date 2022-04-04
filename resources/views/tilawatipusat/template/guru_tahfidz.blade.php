c866e9e46cb22fa214276af8e9efe387833e0cee
FebriTahta <febrirtah@gmail.com>
2022-04-04 09:49:23 +0700

    tahfidz

--- resources/views/tilawatipusat/template/guru_tahfidz.blade.php
+++ resources/views/tilawatipusat/template/guru_tahfidz.blade.php
@@ -1,4 +1,4 @@
-<table>
+{{-- <table>
     <thead style="text-transform: uppercase">
         <tr>
             <th>Nama Peserta</th>
@@ -49,6 +49,52 @@
             <td>29</td>
             <td>20</td>
             <td>93</td>
+        </tr>
+    </tbody>
+</table> --}}
+
+
+<table>
+    <thead style="text-transform: uppercase">
+        <tr>
+            <th>Ya</th>
+            <th>Alamat</th>
+            <th>Kota</th>
+            <th>Phone</th>
+            <th>Tempat Lahir</th>
+            <th>Tanggal Lahir</th>
+            <th>Lembaga</th>
+            <th>Jilid</th>
+            <th>Kriteria</th>
+            <th>Bersyahadah</th>
+            <th>Fashohah</th>
+            <th>Tajwid</th>
+            <th>Ghorib Musykilat</th>
+            <th>Suara dan Lagu</th>
+            <th>MSM</th>
+            <th>MS</th>
+            <th>PK</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>NAMA + Gelar</td>
+            <td>ALAMAT (TANPA KOTA)</td>
+            <td>SURABAYA (TIDAK DISINGKAT)</td>
+            <td>083xxxxxxxxx</td>
+            <td>SURABAYA (TIDAK DISINGKAT)</td>
+            <td>(FORMAT DATE EXCEl YANG BENAR)</td>
+            <td>NAMA LEMBAGA</td>
+            <td>1 (1-5)</td>
+            <td>LULUS SEBAGAI ... </td>
+            <td>1 (UNTUK BERSYAHADAH)</td>
+            <td>26 (contoh "hanya angka")</td>
+            <td>38 (contoh "hanya angka")</td>
+            <td>10 (contoh "hanya angka")</td>
+            <td>5  (contoh "hanya angka")</td>
+            <td>80 (contoh "hanya angka")</td>
+            <td>85 (contoh "hanya angka")</td>
+            <td>90 (contoh "hanya angka")</td>
         </tr>
     </tbody>
 </table>
