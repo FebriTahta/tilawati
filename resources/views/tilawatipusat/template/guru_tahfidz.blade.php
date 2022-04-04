0b852174e6ed6a37952ccd4ddbe978365e22595a
FebriTahta <febrirtah@gmail.com>
2022-04-04 09:50:16 +0700

    tahfidz

--- resources/views/tilawatipusat/template/guru_tahfidz.blade.php
+++ resources/views/tilawatipusat/template/guru_tahfidz.blade.php
@@ -57,7 +57,7 @@
 <table>
     <thead style="text-transform: uppercase">
         <tr>
-            <th>Ya</th>
+            <th>Nama Peserta</th>
             <th>Alamat</th>
             <th>Kota</th>
             <th>Phone</th>
@@ -67,34 +67,28 @@
             <th>Jilid</th>
             <th>Kriteria</th>
             <th>Bersyahadah</th>
-            <th>Fashohah</th>
+            <th>Tahfidz</th>
             <th>Tajwid</th>
-            <th>Ghorib Musykilat</th>
-            <th>Suara dan Lagu</th>
-            <th>MSM</th>
-            <th>MS</th>
-            <th>PK</th>
+            <th>Fashohah</th>
+            <th>Microteaching</th> 
         </tr>
     </thead>
     <tbody>
         <tr>
-            <td>NAMA + Gelar</td>
-            <td>ALAMAT (TANPA KOTA)</td>
-            <td>SURABAYA (TIDAK DISINGKAT)</td>
-            <td>083xxxxxxxxx</td>
-            <td>SURABAYA (TIDAK DISINGKAT)</td>
-            <td>(FORMAT DATE EXCEl YANG BENAR)</td>
+            <td>FEBRI RIZQI TAHTA, S.Kom</td>
+            <td>SIMO JAWAR III / KEC.SUKOMANUNGGAL</td>
+            <td>SURABAYA</td>
+            <td>08132965421</td>
+            <td>SURABAYA</td>
+            <td>6/12/1996</td>
             <td>NAMA LEMBAGA</td>
-            <td>1 (1-5)</td>
-            <td>LULUS SEBAGAI ... </td>
-            <td>1 (UNTUK BERSYAHADAH)</td>
-            <td>26 (contoh "hanya angka")</td>
-            <td>38 (contoh "hanya angka")</td>
-            <td>10 (contoh "hanya angka")</td>
-            <td>5  (contoh "hanya angka")</td>
-            <td>80 (contoh "hanya angka")</td>
-            <td>85 (contoh "hanya angka")</td>
-            <td>90 (contoh "hanya angka")</td>
+            <td>(1-5)</td>
+            <td>SEBAGAI GURU TAHFIDZ AL QURAN METODE TILAWATI</td>
+            <td>1</td>
+            <td>50</td>
+            <td>29</td>
+            <td>20</td>
+            <td>93</td>
         </tr>
     </tbody>
 </table>
