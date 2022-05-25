580a7838df353b47bfae2ddddaee2bf751827085
FebriTahta <febrirtah@gmail.com>
2022-05-25 13:07:50 +0700

    kode cabang

--- app/Http/Controllers/CabangCont.php
+++ app/Http/Controllers/CabangCont.php
@@ -135,7 +135,7 @@ class CabangCont extends Controller
         {
             if(!empty($request->dari))
             {
-                $data   = Cabang::orderBy('created_at','desc')->whereBetween('created_at', array($request->dari, $request->sampai))->with('provinsi','kabupaten','kpa','trainer');
+                $data   = Cabang::orderBy('id','desc')->whereBetween('created_at', array($request->dari, $request->sampai))->with('provinsi','kabupaten','kpa','trainer');
                 return DataTables::of($data)
                 ->addColumn('provinsi', function ($data) {
                     return $data->provinsi->nama;
@@ -207,7 +207,7 @@ class CabangCont extends Controller
                 ->rawColumns(['provinsi','kabupaten','total_kpa','trainer','opsi','tot_lembaga'])
                 ->make(true);
             }else{
-                $data   = Cabang::orderBy('created_at','desc')->whereBetween('created_at', array($request->dari, $request->sampai))->with('provinsi','kabupaten','kpa','trainer');
+                $data   = Cabang::orderBy('created_at','desc')->with('provinsi','kabupaten','kpa','trainer');
                 return DataTables::of($data)
                 ->addColumn('provinsi', function ($data) {
                     if ($data->provinsi == null) {
