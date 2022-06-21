ffb010e87ace819f54ec5ed0f84332b4484607f8
FebriTahta <febrirtah@gmail.com>
2022-06-21 16:30:39 +0700

    cetak depan

--- resources/views/AdmPelatihan/Cetak/cetak_depan.blade.php
+++ resources/views/AdmPelatihan/Cetak/cetak_depan.blade.php
@@ -149,7 +149,7 @@ $date = $item->tgllahir; ?>
                                 @elseif(substr($item->tmptlahir, 5, 4) == 'KAB.')
                                     {{substr($item->tmptlahir, 5)}}
                                 @else
-                                    {{substr($item->tmptlahir, 5)}},
+                                    {{$item->tmptlahir}},
                                 @endif
                                 {{ Carbon\Carbon::parse($date)->isoFormat('D MMMM Y') }}&nbsp;</td>
                         
@@ -164,7 +164,7 @@ $date = $item->tgllahir; ?>
                                 @elseif(substr($item->tmptlahir2, 5, 4) == 'KAB.')
                                     {{substr($item->tmptlahir2, 5)}}
                                 @else
-                                    {{substr($item->tmptlahir2, 5)}},
+                                    {{$item->tmptlahir2}},
                                 @endif
                                 {{ Carbon\Carbon::parse($date)->isoFormat('D MMMM Y') }}&nbsp;</td>
                         @endif
