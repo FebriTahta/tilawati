92a84424aec56765880def7654fef1bdb1a6753e
FebriTahta <febrirtah@gmail.com>
2022-06-29 14:44:01 +0700

    pelatihan online

--- app/Http/Controllers/ApiCont.php
+++ app/Http/Controllers/ApiCont.php
@@ -30,8 +30,9 @@ class ApiCont extends Controller
     {
         $data = Pelatihan::where('cabang_id',79)
         ->where('pendaftaran', null)->orWhere('pendaftaran','dibuka')
+        ->join('pelatihans','flyer.pelatihan_id','pelatihans.id')
         ->join('programs','pelatihans.program_id','programs.id')
-        ->select('pelatihans.slug','programs.name','pelatihans.tanggal','pelatihans.keterangan','pelatihans.jenis')
+        ->select('pelatihans.slug','programs.name','pelatihans.tanggal','pelatihans.keterangan','pelatihans.jenis','flyers.image')
         ->get();
 
         if($data)
