758a77529b2132a505d489a7cbb2555505304886
FebriTahta <febrirtah@gmail.com>
2022-07-19 10:16:51 +0700

    cetak syahadah lama

--- resources/views/tilawatipusat/cetak/depan/lama2.blade.php
+++ resources/views/tilawatipusat/cetak/depan/lama2.blade.php
@@ -44,16 +44,16 @@
                     <td class="atas" style="width: 11px; height: 23px;"></td>
                     <td class="atas" style="width: 750px; height: 23px; text-transform: uppercase" >{{ $item->alamat }} 
 						
-						{{-- @if (substr($item->kota, 4, 4) == 'KOTA ADM.')
-								{{substr($item->kota, 5)}}
-						@elseif(substr($item->kota, 0, 4) == 'KOTA')
+						@if (substr($item->kota, 4, 4) == 'ADM')
 								{{substr($item->kota, 10)}}
+						@elseif(substr($item->kota, 0, 4) == 'KOTA')
+								{{substr($item->kota, 5)}}
 						@elseif(substr($item->kota, 0, 4) == 'KAB.')
 								{{substr($item->kota, 5)}}
 						@else
 								{{substr($item->kota, 5)}},
-						@endif --}}
-						{{substr($item->kota, 4,4)}}
+						@endif
+						{{-- {{substr($item->kota, 4,4)}} --}}
 					
 					</td>
                     <td class="atas" style="width: 52px; height: 23px;">&nbsp;</td>
