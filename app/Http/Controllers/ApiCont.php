910a7910f9e4caa4714a6c1a27df46d60faa5f57
FebriTahta <febrirtah@gmail.com>
2022-07-21 13:41:08 +0700

    jenis posting new backend

--- app/Http/Controllers/ApiCont.php
+++ app/Http/Controllers/ApiCont.php
@@ -70,7 +70,7 @@ class ApiCont extends Controller
         ->orWhere('kepalacabang', 'like', '%'. $search . '%')
         ->orWhere('alamat', 'like', '%'. $search . '%')
         ->orWhere('telp', 'like', '%'. $search . '%')
-        ->paginate(10);
+        ->get();
 
         if($data)
         {
