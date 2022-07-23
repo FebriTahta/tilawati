2385670369ceed44a20c0f3c367734648641bc77
FebriTahta <febrirtah@gmail.com>
2022-07-23 15:13:10 +0700

    cetak syahadah

--- resources/views/tilawatipusat/cetak/depan/lama.blade.php
+++ resources/views/tilawatipusat/cetak/depan/lama.blade.php
@@ -69,7 +69,7 @@
 							@elseif(substr($item->tmptlahir, 0, 4) == 'KAB.')
 								{{substr($item->tmptlahir, 5)}}
 							@else
-								{{$item->tmptlahir}},
+								{{$item->tmptlahir}}
 							@endif
 						@endif
 
@@ -81,7 +81,7 @@
 							@elseif(substr($item->tmptlahir2, 0, 4) == 'KAB.')
 								{{substr($item->tmptlahir2, 5)}}
 							@else
-								{{$item->tmptlahir2}},
+								{{$item->tmptlahir2}}
 							@endif
 						@endif
 						
