fd55e73bf6c4742bba2732ac47877b9b6f959fe9
FebriTahta <febrirtah@gmail.com>
2022-08-12 11:02:33 +0700

    id pada program

--- app/Http/Controllers/ErrorCont.php
+++ app/Http/Controllers/ErrorCont.php
@@ -28,13 +28,41 @@ class ErrorCont extends Controller
 
     public function fixing()
     {
-        $x26 = Nilai::where('penilaian_id',26)->count();
-        $x27 = Nilai::where('penilaian_id',27)->count();
-        $x28 = Nilai::where('penilaian_id',28)->count();
-        $x29 = Nilai::where('penilaian_id',29)->count();
-        $x30 = Nilai::where('penilaian_id',30)->count();
-        $x31 = Nilai::where('penilaian_id',31)->count();
-        $x32 = Nilai::where('penilaian_id',32)->count();
+        $x26 = Nilai::where('penilaian_id',26)->update(
+            [
+                'penilaian_id'=>33
+            ]
+        );
+        $x27 = Nilai::where('penilaian_id',27)->update(
+            [
+                'penilaian_id'=>34
+            ]
+        );
+        $x28 = Nilai::where('penilaian_id',28)->update(
+            [
+                'penilaian_id'=>35
+            ]
+        );;
+        $x29 = Nilai::where('penilaian_id',29)->update(
+            [
+                'penilaian_id'=>36
+            ]
+        );
+        $x30 = Nilai::where('penilaian_id',30)->update(
+            [
+                'penilaian_id'=>37
+            ]
+        );
+        $x31 = Nilai::where('penilaian_id',31)->update(
+            [
+                'penilaian_id'=>38
+            ]
+        );
+        $x32 = Nilai::where('penilaian_id',32)->update(
+            [
+                'penilaian_id'=>39
+            ]
+        );
 
 
         return $x26.' - '.$x27.' - '.$x28.' - '.$x29.' - '.$x30.' - '.$x31.' - '.$x32;
