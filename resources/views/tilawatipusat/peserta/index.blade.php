5f21b88f55f278e63cf11d86435b4aa039933593
FebriTahta <febrirtah@gmail.com>
2022-08-22 09:14:15 +0700

    tot

--- resources/views/tilawatipusat/peserta/index.blade.php
+++ resources/views/tilawatipusat/peserta/index.blade.php
@@ -257,7 +257,7 @@
                                 <tr>
                                     <th>id</th>
                                     <th style="5%">Pilih</th>
-                                    @if ($diklat->program->name == "Diklat Munaqisy Cabang")
+                                    @if ($diklat->program->name == "Diklat Munaqisy Cabang" || $diklat->program->name == "Training Of Trainer Guru Al-Qur'an")
                                         <th>Asal Cabang</th>
                                     @endif
                                     <th>Peserta</th>
