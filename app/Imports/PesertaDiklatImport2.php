4c16d9aac2ca651d48fd2a610dc375e541ef2650
FebriTahta <febrirtah@gmail.com>
2022-08-26 15:01:00 +0700

    template tot

--- app/Imports/PesertaDiklatImport2.php
+++ app/Imports/PesertaDiklatImport2.php
@@ -75,106 +75,106 @@ class PesertaDiklatImport2 implements ToCollection, WithStartRow
                             
                             # code...
     
-                            $dt_pel = new Peserta;
-                            $dt_pel->phonegara_id = 175;
-                            $dt_pel->pelatihan_id = $this->id;
-                            $dt_pel->program_id = $diklat->program_id;
-                            $dt_pel->cabang_id = $this->cabang_id;
-                            $dt_pel->tanggal = $this->tanggal;
-                            $dt_pel->asal_cabang = [0];
-                            $dt_pel->name = $row[1];
-                            $dt_pel->alamat = $row[2];
-                            $dt_pel->kota = $row[3];
-                            $dt_pel->status =1;
-                            //slug
-                            $slug = Str::slug($row[1].'-'.$diklat->program->name.'-'.Carbon::parse($this->tanggal)->isoFormat('MMMM-D-Y').'-'.$diklat->cabang->name.'-'.$diklat->cabang->kabupaten->nama);
-                            $dt_pel->slug = $slug;
-                            //inisialisasi kota / kabupaten yang diinput
+                            // $dt_pel = new Peserta;
+                            // $dt_pel->phonegara_id = 175;
+                            // $dt_pel->pelatihan_id = $this->id;
+                            // $dt_pel->program_id = $diklat->program_id;
+                            // $dt_pel->cabang_id = $this->cabang_id;
+                            // $dt_pel->tanggal = $this->tanggal;
+                            // $dt_pel->asal_cabang = [0];
+                            // $dt_pel->name = $row[1];
+                            // $dt_pel->alamat = $row[2];
+                            // $dt_pel->kota = $row[3];
+                            // $dt_pel->status =1;
+                            // //slug
+                            // $slug = Str::slug($row[1].'-'.$diklat->program->name.'-'.Carbon::parse($this->tanggal)->isoFormat('MMMM-D-Y').'-'.$diklat->cabang->name.'-'.$diklat->cabang->kabupaten->nama);
+                            // $dt_pel->slug = $slug;
+                            // //inisialisasi kota / kabupaten yang diinput
     
-                            $kab     = strtoupper($row[3]);
-                            $kab_kab = 'KAB. '.$kab;
-                            $kab_kot = 'KOTA '.$kab;
-                            $tes_kab = Kabupaten::select('*')->whereIn('nama',[$kab_kab])->first();
-                            $tes_kot = Kabupaten::select('*')->whereIn('nama',[$kab_kot])->first();
-                            //proses logika untuk mendapatkan kabupaten id & menginput provinsi id otomatis dari kabupaten
-                            if ($tes_kab !== null) {
-                                # code...
-                                $kabupaten_id = $tes_kab->id;
-                                $dt_pel->kabupaten_id = $kabupaten_id;
-                                $dt_pel->provinsi_id = $tes_kab->provinsi->id;
-                            } 
-                            if ($tes_kot !== null) {
-                                # code...
-                                $kabupaten_id = $tes_kot->id;
-                                $dt_pel->kabupaten_id = $kabupaten_id;
-                                $dt_pel->provinsi_id = $tes_kot->provinsi->id;
-                            }
+                            // $kab     = strtoupper($row[3]);
+                            // $kab_kab = 'KAB. '.$kab;
+                            // $kab_kot = 'KOTA '.$kab;
+                            // $tes_kab = Kabupaten::select('*')->whereIn('nama',[$kab_kab])->first();
+                            // $tes_kot = Kabupaten::select('*')->whereIn('nama',[$kab_kot])->first();
+                            // //proses logika untuk mendapatkan kabupaten id & menginput provinsi id otomatis dari kabupaten
+                            // if ($tes_kab !== null) {
+                            //     # code...
+                            //     $kabupaten_id = $tes_kab->id;
+                            //     $dt_pel->kabupaten_id = $kabupaten_id;
+                            //     $dt_pel->provinsi_id = $tes_kab->provinsi->id;
+                            // } 
+                            // if ($tes_kot !== null) {
+                            //     # code...
+                            //     $kabupaten_id = $tes_kot->id;
+                            //     $dt_pel->kabupaten_id = $kabupaten_id;
+                            //     $dt_pel->provinsi_id = $tes_kot->provinsi->id;
+                            // }
     
-                            $phone = $row[4];
+                            // $phone = $row[4];
     
-                            if (substr($phone,0,1) == '0') {
-                                # jika awalan angkanya 0 dari export maka langsung simpan
-                                # code...
-                                $telephone = $phone;
-                                $dt_pel->telp = $telephone;
+                            // if (substr($phone,0,1) == '0') {
+                            //     # jika awalan angkanya 0 dari export maka langsung simpan
+                            //     # code...
+                            //     $telephone = $phone;
+                            //     $dt_pel->telp = $telephone;
                                 
-                            }else{
-                                # baca jika awalan tidak 0 melainkan 62 
-                                if (substr($phone,0,2) == '62'){
-                                    # code...
-                                    $telephone = $phone;
-                                    $dt_pel->telp = $telephone;
-                                }else{
-                                    // $potong = substr($phone,1,15);
-                                    $telephone = '0'.$phone;
-                                    $dt_pel->telp = $telephone;   
-                                }
-                            }
-                            $dt_pel->tmptlahir = $row[5];
+                            // }else{
+                            //     # baca jika awalan tidak 0 melainkan 62 
+                            //     if (substr($phone,0,2) == '62'){
+                            //         # code...
+                            //         $telephone = $phone;
+                            //         $dt_pel->telp = $telephone;
+                            //     }else{
+                            //         // $potong = substr($phone,1,15);
+                            //         $telephone = '0'.$phone;
+                            //         $dt_pel->telp = $telephone;   
+                            //     }
+                            // }
+                            // $dt_pel->tmptlahir = $row[5];
     
-                            if (is_numeric($row[6]) !== false) {
-                                # code...
-                                $masuk = \PhpOffice\PhpSpreadsheet\Shared\Date::excelToDateTimeObject($row[6]);
-                                $dt_pel->tgllahir=$masuk;
-                            }
+                            // if (is_numeric($row[6]) !== false) {
+                            //     # code...
+                            //     $masuk = \PhpOffice\PhpSpreadsheet\Shared\Date::excelToDateTimeObject($row[6]);
+                            //     $dt_pel->tgllahir=$masuk;
+                            // }
                             
-                            $dt_pel->kriteria=$row[7];
+                            // $dt_pel->kriteria=$row[7];
     
-                            $kriteria = Kriteria::where('name',$row[7])->first();
-                            if ($kriteria !== null) {
-                                # code...
-                                $dt_pel->kriteria_id = $kriteria->id;
-                            }
-                            $dt_pel->bersyahadah = $row[8];
+                            // $kriteria = Kriteria::where('name',$row[7])->first();
+                            // if ($kriteria !== null) {
+                            //     # code...
+                            //     $dt_pel->kriteria_id = $kriteria->id;
+                            // }
+                            // $dt_pel->bersyahadah = $row[8];
     
-                            $dt_pel->created_at = new \DateTime;
+                            // $dt_pel->created_at = new \DateTime;
     
     
-                            $dt_pel->save();
+                            // $dt_pel->save();
     
-                            foreach ( $dt_pel->pelatihan->program->penilaian as $key => $value) {
-                                # code...
-                                if ($row[$key+9] !== null) {
-                                    # code...
-                                    $dt_n = new Nilai;
-                                    $dt_n->peserta_id = $dt_pel->id;
-                                    $dt_n->penilaian_id=$value->id;
-                                    $dt_n->nominal=$row[$key+9];
-                                    $dt_n->kategori=$value->kategori;
-                                    $dt_n->save();
-                                }else {
-                                    # code...
-                                    $dt_n = new Nilai;
-                                    $dt_n->peserta_id = $dt_pel->id;
-                                    $dt_n->penilaian_id=$value->id;
-                                    $dt_n->nominal='0';
-                                    $dt_n->kategori=$value->kategori;
-                                    $dt_n->save();
-                                }
+                            // foreach ( $dt_pel->pelatihan->program->penilaian as $key => $value) {
+                            //     # code...
+                            //     if ($row[$key+9] !== null) {
+                            //         # code...
+                            //         $dt_n = new Nilai;
+                            //         $dt_n->peserta_id = $dt_pel->id;
+                            //         $dt_n->penilaian_id=$value->id;
+                            //         $dt_n->nominal=$row[$key+9];
+                            //         $dt_n->kategori=$value->kategori;
+                            //         $dt_n->save();
+                            //     }else {
+                            //         # code...
+                            //         $dt_n = new Nilai;
+                            //         $dt_n->peserta_id = $dt_pel->id;
+                            //         $dt_n->penilaian_id=$value->id;
+                            //         $dt_n->nominal='0';
+                            //         $dt_n->kategori=$value->kategori;
+                            //         $dt_n->save();
+                            //     }
                                     
-                            }
+                            // }
                             
-                            $id = $dt_pel->id;
+                            // $id = $dt_pel->id;
                         }else{
                             
                             // UPDATE DENGAN ASAL CABANG
