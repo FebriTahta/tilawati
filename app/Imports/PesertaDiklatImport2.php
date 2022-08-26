a655069653642da2413cb9d6bc548896e0da8c2a
FebriTahta <febrirtah@gmail.com>
2022-08-26 15:02:13 +0700

    template tot

--- app/Imports/PesertaDiklatImport2.php
+++ app/Imports/PesertaDiklatImport2.php
@@ -81,7 +81,7 @@ class PesertaDiklatImport2 implements ToCollection, WithStartRow
                             $dt_pel->program_id = $diklat->program_id;
                             $dt_pel->cabang_id = $this->cabang_id;
                             $dt_pel->tanggal = $this->tanggal;
-                            $dt_pel->asal_cabang = [0];
+                            // $dt_pel->asal_cabang = [0];
                             $dt_pel->name = $row[1];
                             $dt_pel->alamat = $row[2];
                             $dt_pel->kota = $row[3];
