97bfab052fa5edbcee83fe0b46b5dfdd168a4ad8
FebriTahta <febrirtah@gmail.com>
2022-08-29 09:25:06 +0700

    max min

--- resources/views/AdmPelatihan/Cetak/cetak_belakang_guru.blade.php
+++ resources/views/AdmPelatihan/Cetak/cetak_belakang_guru.blade.php
@@ -520,11 +520,11 @@
 					<tr>
 							<th rowspan="2">No.</th>
 							<th rowspan="2" style="border-right: none; text-align: right">Bidang Penilaian</th>
-							<th colspan="3" class="penilaian" style="border-bottom: none; "></th>
+							<th colspan="3" class="penilaian" style="border-bottom: none; border-left: none;"></th>
 							<th rowspan="2" style="text-align: center">Jumlah</th>
 					</tr>
 					<tr>     
-						<th class="pe"></th>
+						<th ></th>
 						<th class="pe">MAX</th>
 						<th class="pe">MIN</th>
 						
