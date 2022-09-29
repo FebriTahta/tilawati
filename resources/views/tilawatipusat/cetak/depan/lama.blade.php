9d6e70bbecf92a049e9bf1dae6f53ce552e4ed55
FebriTahta <febrirtah@gmail.com>
2022-09-29 16:26:47 +0700

    kacab kalimantan timur disingkat kaltim

--- resources/views/tilawatipusat/cetak/depan/lama.blade.php
+++ resources/views/tilawatipusat/cetak/depan/lama.blade.php
@@ -266,7 +266,7 @@
 									@elseif($item->pelatihan->cabang->name == 'Tilawati Citra Anak Sholeh')
 										Kacab. CAS Surabaya Jawa Timur
 									@elseif($item->pelatihan->cabang->name == 'Al Kautsar')
-										<span style="font-size: 12px">{{ 'Kacab. '.ucfirst($item->pelatihan->cabang->name).' '.ucfirst($provinsi) }}</span>
+										<span style="font-size: 12px">{{ 'Kacab. '.ucfirst($item->pelatihan->cabang->name).' '.ucfirst($prov) }}</span>
 									@else
 										{{ 'Kacab. '.ucfirst($kab).' '.ucfirst($prov)}}
 									@endif
