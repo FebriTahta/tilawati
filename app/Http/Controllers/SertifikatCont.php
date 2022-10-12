96e4770241755f1ec2332290367d04d0550f83cb
FebriTahta <febrirtah@gmail.com>
2022-10-12 15:56:35 +0700

    sertifikat

--- app/Http/Controllers/SertifikatCont.php
+++ app/Http/Controllers/SertifikatCont.php
@@ -144,7 +144,7 @@ class SertifikatCont extends Controller
                 ->rawColumns(['cabang'])
                 ->make(true);
             }else{
-                $data   = Induksertifikat::with('cabang','certificate')->orderBy('tgl_awal','desc');
+                $data   = Induksertifikat::with('cabang','certificate');
                 return DataTables::of($data)
                     ->addColumn('cabang', function ($data) {
                         return $data->cabang->name;
