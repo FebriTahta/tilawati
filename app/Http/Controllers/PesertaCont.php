45e94857433cc44e5e39c1a940c34832c44074c2
FebriTahta <febrirtah@gmail.com>
2022-11-18 13:45:09 +0700

    jenis program

--- app/Http/Controllers/PesertaCont.php
+++ app/Http/Controllers/PesertaCont.php
@@ -2757,10 +2757,10 @@ class PesertaCont extends Controller
                 })->distinct()->get();
                 return DataTables::of($data)
                 ->addColumn('total', function ($data) use ($dari, $sampai)  {
-                    return $data->pelatihan->whereBetween('tanggal', array($dari, $sampai))->count().' - '.$data->jenisprogram;
+                    return "<pre>$data->pelatihan->whereBetween('tanggal', array($dari, $sampai))->count().' - Diklat'</pre>";
                 })
                 ->addColumn('totalpeserta', function ($data) use ($dari, $sampai){
-                    return $data->peserta->whereBetween('tanggal', array($dari, $sampai))->count().' - '.$data->jenisprogram;
+                    return "<pre>$data->peserta->whereBetween('tanggal', array($dari, $sampai))->count().' - '.$data->jenisprogram</pre>";
                 })
                 ->rawColumns(['total','totalpeserta'])
                 ->make(true);
@@ -2772,10 +2772,10 @@ class PesertaCont extends Controller
                 })->distinct()->get();
                 return DataTables::of($data)
                 ->addColumn('total', function ($data) {
-                    return $data->pelatihan->count().' - '.$data->jenisprogram;
+                    return "<pre>$data->pelatihan->count().' - Diklat'</pre>";
                 })
                 ->addColumn('totalpeserta', function ($data){
-                    return $data->peserta->count().' - '.$data->jenisprogram;
+                    return "<pre>$data->peserta->count().' - '.$data->jenisprogram</pre>";
                 })
                 ->rawColumns(['total','totalpeserta'])
                 ->make(true);
