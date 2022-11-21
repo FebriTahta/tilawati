7c6acb95c829f601c0a3027525c5544a46f90127
FebriTahta <febrirtah@gmail.com>
2022-11-21 16:40:58 +0700

    cetak balakang split 2 bagian karena kebanyakan

--- app/Http/Controllers/CetakCont.php
+++ app/Http/Controllers/CetakCont.php
@@ -322,7 +322,7 @@ class CetakCont extends Controller
         // $customPaper = array(0,0,792,612);
     	// $pdf = PDF::loadview('AdmPelatihan.Cetak.cetak_belakang_guru',compact('peserta','pelatihan'))->setPaper($customPaper, 'portrait');
     	// return $pdf->download('ijazah-belakang-peserta-_'.$pelatihan->name.'.pdf','I');
-        return $peserta_x->count().' & '.$peserta->count().' & bagian '.$bagian_1;
+        return $peserta_x->count().' & '.$peserta->count().' & bagian '.$bagian_1.' - '.$reqeust->pelatihan_id;
     }
 
     public function cetak_belakang_bagian_2(Request $request)
