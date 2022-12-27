027befb7f0c287a068a7c69ddcfd853bc0fa28f6
FebriTahta <febrirtah@gmail.com>
2022-12-27 14:24:52 +0700

    commit new import lembaga

--- app/Imports/LembagaImport.php
+++ app/Imports/LembagaImport.php
@@ -45,9 +45,92 @@ class LembagaImport implements ToCollection, WithChunkReading
 
                 if ($template_nama !== 'Nama') {
                     # code...
-                    if ($data == null) {
-                        # code...
-                        $lembaga = new Lembaga;
+                    // if ($data == null) {
+                    //     # code...
+                    //     $lembaga = new Lembaga;
+                    //     $lembaga->kode = $hasil;
+                    //     $lembaga->cabang_id = $this->cabang_id;
+                    //     $lembaga->name = $row[1];
+                    //     $lembaga->kepalalembaga = $row[2];
+                    //     $lembaga->jenjang = $row[3];
+                    //     $lembaga->telp = $row[4];
+
+                    //     if ($row[5] !== null) {
+                    //         # code...
+                    //         $kab     = strtoupper($row[5]);
+                    //         $kab_kab = 'KAB. '.$kab;
+                    //         $kab_kot = 'KOTA '.$kab;
+                    //         $tes_kab = Kabupaten::select('*')->whereIn('nama',[$kab_kab])->first();
+                    //         $tes_kot = Kabupaten::select('*')->whereIn('nama',[$kab_kot])->first();
+                    //         //proses logika untuk mendapatkan kabupaten id & menginput provinsi id otomatis dari kabupaten
+                    //         if ($tes_kab !== null) {
+                    //             # code...
+                    //             $kabupaten_id = $tes_kab->id;
+                    //             $lembaga->kabupaten_id = $kabupaten_id;
+                    //             $lembaga->provinsi_id = $tes_kab->provinsi->id;
+                    //         } 
+                    //         if ($tes_kot !== null) {
+                    //             # code...
+                    //             $kabupaten_id = $tes_kot->id;
+                    //             $lembaga->kabupaten_id = $kabupaten_id;
+                    //             $lembaga->provinsi_id = $tes_kot->provinsi->id;
+                    //         }
+                    //     }
+
+                    //     $lembaga->jml_guru = $row[6];
+                    //     $lembaga->jml_santri = $row[7];
+                    //     $lembaga->alamat = $row[8];
+                    //     $lembaga->pengelola = $row[9];
+                    //     $lembaga->status = $row[10];
+                    //     $lembaga->created_at = new \DateTime;
+                    //     $lembaga->save();
+                    // }else {
+                    //     # code...
+                    //     $kabupaten_id = null;
+                    //     if ($row[5] !== null) {
+                    //         # code...
+                    //         $kab     = strtoupper($row[5]);
+                    //         $kab_kab = 'KAB. '.$kab;
+                    //         $kab_kot = 'KOTA '.$kab;
+                    //         $tes_kab = Kabupaten::select('*')->whereIn('nama',[$kab_kab])->first();
+                    //         $tes_kot = Kabupaten::select('*')->whereIn('nama',[$kab_kot])->first();
+                    //         //proses logika untuk mendapatkan kabupaten id & menginput provinsi id otomatis dari kabupaten
+                    //         if ($tes_kab !== null) {
+                    //             # code...
+                    //             $kabupaten_id = $tes_kab->id;
+                    //             // $lembaga->kabupaten_id = $kabupaten_id;
+                    //             // $lembaga->provinsi_id = $tes_kab->provinsi->id;
+                    //         } 
+                    //         if ($tes_kot !== null) {
+                    //             # code...
+                    //             $kabupaten_id = $tes_kot->id;
+                    //             // $lembaga->kabupaten_id = $kabupaten_id;
+                    //             // $lembaga->provinsi_id = $tes_kot->provinsi->id;
+                    //         }
+                    //     }
+
+                    //     $cabang = Cabang::where('id',$this->cabang_id)->first();
+                    //     $lembaga = Lembaga::updateOrCreate(
+                    //         [
+                    //             'id'=> $data->id,
+                    //         ],
+                    //         [
+                    //             'name' => $row[1],
+                    //             'kepalalembaga' => $row[2],
+                    //             'jenjang' => $row[3],
+                    //             'telp' => $row[4],
+                    //             'kabupaten_id'=> $kabupaten_id,
+                    //             'jml_guru' => $row[6],
+                    //             'jml_santri' => $row[7],
+                    //             'alamat' => $row[8],
+                    //             'pengelola' => $row[9],
+                    //             'status' => $row[10],
+                    //         ]
+                    //     );
+    
+                    // }
+
+                    $lembaga = new Lembaga;
                         $lembaga->kode = $hasil;
                         $lembaga->cabang_id = $this->cabang_id;
                         $lembaga->name = $row[1];
@@ -84,51 +167,6 @@ class LembagaImport implements ToCollection, WithChunkReading
                         $lembaga->status = $row[10];
                         $lembaga->created_at = new \DateTime;
                         $lembaga->save();
-                    }else {
-                        # code...
-                        $kabupaten_id = null;
-                        if ($row[5] !== null) {
-                            # code...
-                            $kab     = strtoupper($row[5]);
-                            $kab_kab = 'KAB. '.$kab;
-                            $kab_kot = 'KOTA '.$kab;
-                            $tes_kab = Kabupaten::select('*')->whereIn('nama',[$kab_kab])->first();
-                            $tes_kot = Kabupaten::select('*')->whereIn('nama',[$kab_kot])->first();
-                            //proses logika untuk mendapatkan kabupaten id & menginput provinsi id otomatis dari kabupaten
-                            if ($tes_kab !== null) {
-                                # code...
-                                $kabupaten_id = $tes_kab->id;
-                                // $lembaga->kabupaten_id = $kabupaten_id;
-                                // $lembaga->provinsi_id = $tes_kab->provinsi->id;
-                            } 
-                            if ($tes_kot !== null) {
-                                # code...
-                                $kabupaten_id = $tes_kot->id;
-                                // $lembaga->kabupaten_id = $kabupaten_id;
-                                // $lembaga->provinsi_id = $tes_kot->provinsi->id;
-                            }
-                        }
-
-                        $cabang = Cabang::where('id',$this->cabang_id)->first();
-                        $lembaga = Lembaga::updateOrCreate(
-                            [
-                                'id'=> $data->id,
-                            ],
-                            [
-                                'name' => $row[1],
-                                'kepalalembaga' => $row[2],
-                                'jenjang' => $row[3],
-                                'telp' => $row[4],
-                                'kabupaten_id'=> $kabupaten_id,
-                                'jml_guru' => $row[6],
-                                'jml_santri' => $row[7],
-                                'alamat' => $row[8],
-                                'pengelola' => $row[9],
-                                'status' => $row[10],
-                            ]
-                        );
-    
-                    }
                 }
                 
             }
