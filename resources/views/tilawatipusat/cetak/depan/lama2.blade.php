dc3d097366ff5a2eb538c79f919b1dcc27e368ae
FebriTahta <febrirtah@gmail.com>
2023-01-24 22:07:20 +0700

    cabang role pusat tanpa syirkah

--- resources/views/tilawatipusat/cetak/depan/lama2.blade.php
+++ resources/views/tilawatipusat/cetak/depan/lama2.blade.php
@@ -210,7 +210,8 @@
 					{{-- direktur --}}
 					<td class="atas" style="width: 241px; height: 5px;"> 
 						<u>
-						@if ($item->pelatihan->cabang->name == 'Cahaya Amanah' || $item->pelatihan->cabang->name == 'Tilawati Pusat' || $item->pelatihan->cabang->status == "RPQ")
+						@if ($item->pelatihan->cabang->name == 'Cahaya Amanah' || $item->pelatihan->cabang->name == 'Tilawati Pusat' || $item->pelatihan->cabang->status == "RPQ"
+						|| $item->pelatihan->cabang->name == 'Tilawati Lombok')
 						Dr. KH. Umar Jaeni, M.Pd
 						@else
 						{{$item->pelatihan->cabang->kepalacabang}}
